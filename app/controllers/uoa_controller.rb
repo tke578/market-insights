ff01cb39a622ca4568b220cdbcfe02dc6989f23f
Andrew G <tke578@gmail.com>
2020-09-13 23:22:40 -0700

    Adds datepicker search

--- app/controllers/uoa_controller.rb
+++ app/controllers/uoa_controller.rb
@@ -5,11 +5,22 @@ class UoaController < ApplicationController
 
 	def index
 		options = UOA.new()
-		options.recent_trades
+
+		if uoa_params
+			options.search(uoa_params)
+		else
+			options.recent_trades
+		end
 		@collection = options.data
 		respond_to  do |format|
 			format.html
 			format.csv { send_data options.to_csv, filename: "UOA_"+Date.today.strftime("%m%d%Y")+'.csv' }
 		end
+	end
+
+	private
+
+	def uoa_params
+		params.permit(:date, :commit, :format)
 	end
 end
--- lib/uoa.rb
+++ lib/uoa.rb
@@ -6,9 +6,10 @@ Mongo::Logger.logger.level = Logger::FATAL
 class UOA
 
 	attr_accessor :data
+	attr_reader	:collection
 
 	def initialize()
-		client 		= Mongo::Client.new(ENV['MONGO_URI'])
+		client 		= Mongo::Client.new(ENV["MONGO_URI"])
 		@collection = client[:uoa]
 		@data 		= []
 	end
@@ -20,10 +21,20 @@ class UOA
 		query.each { |r| @data.push(r) }
 	end
 
+	def search(params)
+		query = {}
+		if params.has_key?(:date)
+			date_parsed = Date.strptime(params[:date], '%m/%d/%Y')
+			condition = { 'created_at': { '$gt': date_parsed.beginning_of_day, '$lt': date_parsed.end_of_day } }
+			query.merge!(condition)
+		end
+		response = @collection.find(query)
+		response.each { |r| @data.push(r) }
+	end
+
 	def to_csv
 		return if @data.blank?
 		row_headers = @data.first.keys().select { |key| !['_id', 'created_at', 'updated_at'].include? key }
-		print(row_headers)
 		CSV.generate(headers: true) do |csv|
 			csv << row_headers
 			@data.each { |record| csv << record }
